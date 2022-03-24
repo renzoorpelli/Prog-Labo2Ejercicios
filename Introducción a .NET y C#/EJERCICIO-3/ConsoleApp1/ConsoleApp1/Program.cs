9147247ce00412d9ec739d26ed66e4ba1c3e4aec
Renzo Orpelli <orpellirenzo@gmail.com>
2022-03-24 13:59:45 -0300

    carpetas agregadas

---
+++ Introducción a .NET y C#/EJERCICIO-3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            bool numeroValido;
+            string continuar;
+
+            do
+            {
+                Console.WriteLine("Ingrese un numero");
+                numeroValido = int.TryParse(Console.ReadLine(), out numero);
+                while (!numeroValido)
+                {
+                    Console.WriteLine("Ingrese un numero");
+                    numeroValido = int.TryParse(Console.ReadLine(), out numero);
+
+                }
+                esPrimo(numero);
+                Console.WriteLine("Desea ingresar otro numero? de lo contrario ingrese 'salir'");
+                continuar = Console.ReadLine();
+             
+            } while (continuar != "salir");
+
+        }
+
+            static void esPrimo(int num1) 
+            {
+                bool todoOk = true;
+                for (int i = 2; i<=num1; i++)
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if(i % j == 0)
+                        {
+                            todoOk = false;
+                            break;
+                        }
+
+                    }
+                    if (todoOk)
+                    {
+                        Console.WriteLine($"El numero {i} es primo");
+                    }
+                    todoOk = true;  
+                }
+            }
+
+        }
+ }
+
+    
+
---
+++ Introducción a .NET y C#/EJERCICIO-5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+// ejercicio 5
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado;
+
+            Console.WriteLine("Ingrese numero");
+            
+            while(int.TryParse(Console.ReadLine(), out numeroIngresado))
+            {
+                for (int i = 1; i < numeroIngresado; i++)// itero desde el 1 hasta el numero ingresado
+                {
+                    int sumadorInferior = 0; // creo variable para los inferiores al centro
+                    for (int j = 0; j < i; j++)// recorro los inferiores
+                    {
+                        sumadorInferior += j;// summo los inferiores al centro
+
+                    }
+
+
+                    int sumadorSuperior = 0;// creo variable para los superiores al centro
+                    for (int k = i + 1; k < sumadorInferior; k++)// reccorro los superirores
+                    {
+                        if ((sumadorInferior == sumadorSuperior) || (sumadorSuperior > sumadorInferior))
+                            break;
+
+                        sumadorSuperior += k;
+                    }
+                    // cuando sean igulaes muestros
+                    if (sumadorInferior == sumadorSuperior)
+                    {
+                        Console.WriteLine($"Es un centro numerico de {i}");
+                    }
+
+                }
+            }
+            Console.ReadKey();
+           
+            
+        }  
+    }
+}
