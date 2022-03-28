179a52f1e10f0a6ac4d4659824250749adcee9cf
Renzo Orpelli <orpellirenzo@gmail.com>
2022-03-28 17:45:36 -0300

    ejercicios realizados en curso

---
+++ Introducción a .NET y C#/EJERCICIO-8/i08/i08/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace i08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Ingrese el numero de filas ");
+            int.TryParse(Console.ReadLine(), out numero);
+
+            for (int i = 1; i <= numero; i++)
+            {
+                for (int j = i; j <= numero; j++)
+                {
+                    Console.Write(" ");
+                    
+                }
+                for (int j = 1; j <= (2*i -1) ; j++)
+                {
+                    Console.Write("*");
+                }
+                
+                Console.WriteLine("");
+            }
+        }
+    }
+}
