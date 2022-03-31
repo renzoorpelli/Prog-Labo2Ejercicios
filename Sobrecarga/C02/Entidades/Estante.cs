5ec14dfef15f391af8ec6da47d9da7f6e5b7421a
Renzo Orpelli <orpellirenzo@gmail.com>
2022-03-31 09:10:48 -0300

    primer commit

---
+++ Sobrecarga/C02/Entidades/Estante.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Estante
+    {
+        Producto[] productos;
+        int ubicacion;
+
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacion):this(capacidad)
+        {
+            this.ubicacion = ubicacion;
+        }
+    }
+}
---
+++ Sobrecarga/i01/i01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Entidades;
+namespace i01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador sumador = new Sumador();
+            Sumador sumador2 = new Sumador(2);
+            // muestra la suma
+            Console.WriteLine(sumador.Sumar(45, 2));
+            //muestra la concatenacion de strings
+            Console.WriteLine(sumador.Sumar("renzo ", "orpelli"));
+
+
+
+            // muestra la cantidad de sumas que hizo el metodo
+            int cantidadSumas = (int)sumador;
+            Console.WriteLine(cantidadSumas);
+
+            //suma la cantidad de sumas de ambos metodos
+            Console.WriteLine(sumador + sumador2);
+            // compara el contenido
+            Console.WriteLine(sumador | sumador2); 
+        }
+    }
+}
