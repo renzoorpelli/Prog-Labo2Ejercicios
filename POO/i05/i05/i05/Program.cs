6c2998c5c109d5e8240289889d0c45d023c3bd22
Renzo Orpelli <orpellirenzo@gmail.com>
2022-04-02 13:10:09 -0300

    mas ejercicios

---
+++ POO/i05/i05/i05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Entidades;
+
+namespace i05
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Punto punto1 = new Punto(2, 1);
+            Punto punto2 = new Punto(3, 4);
+            Rectangulo rec1 = new Rectangulo(punto1, punto2);
+
+            Console.WriteLine(MostrarRectangulo(rec1));
+        }
+
+        static string MostrarRectangulo(Rectangulo rectangulo)
+        {
+            return $"El Area : {rectangulo.Area()} y  el Perimetro : {rectangulo.Perimetro()}";
+        }
+
+    }
+}
