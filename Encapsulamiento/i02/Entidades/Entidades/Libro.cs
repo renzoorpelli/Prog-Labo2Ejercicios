ecefc4651bf3db90ff18b9445cdd45317a6ee6f7
Renzo Orpelli <orpellirenzo@gmail.com>
2022-05-05 16:20:45 -0300

    mas ejercicios

---
+++ Encapsulamiento/i02/Entidades/Entidades/Libro.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Entidades
+{
+    public class Libro
+    {
+        private List<string> paginas;
+
+        public Libro()
+        {
+            this.paginas = new List<string>();
+        }
+        
+        public string this[int i]
+        {
+            get
+            {
+                if(i >= 0 && i < paginas.Count)
+                {
+                    return paginas[i];
+                }
+                return "Pagina no encontrada";
+            }
+
+            set
+            {
+                if(i> paginas.Count)
+                {
+                    this.paginas.Add(value);
+                }
+                else if(i >= 0)
+                {
+                    this.paginas.Insert(i, value);
+                }
+            }
+        }
+    }
+}
---
+++ Encapsulamiento/i02/Entidades/libros/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Entidades;
+namespace libros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro1 = new Libro();
+
+            libro1[0] = "pepito iv";
+            libro1[1] = "juancito v";
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(libro1[i]);
+            }
+
+            libro1[1] = "juancito v";
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(libro1[i]);
+            }
+
+        }
+    }
+}
